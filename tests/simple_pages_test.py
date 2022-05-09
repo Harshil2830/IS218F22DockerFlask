84d87733d510abd241d21d45b1922ab8093ddea6
Harshil2830 <shahharshil2830@gmail.com>
2022-05-08 23:49:27 -0400

    Fixing simple pages test

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -6,6 +6,10 @@ def test_request_main_menu_links(client):
     response = client.get("/")
     assert response.status_code == 200
 #   assert b'<a class="nav-link" href="/about">About</a>' in response.data
+    assert b'href="/about"' in response.data
+    assert b'href="/welcome"' in response.data
+    assert b'href="/login"' in response.data
+    assert b'href="/register"' in response.data
 
 
 def test_request_index(client):
