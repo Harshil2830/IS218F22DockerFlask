31be05b50f8c50f9a44dd906f353e7fc71a736f6
Harshil2830 <shahharshil2830@gmail.com>
2022-05-09 17:10:43 -0400

    Fixed logging

--- app/__init__.py
+++ app/__init__.py
@@ -13,8 +13,8 @@ from app.cli import create_database
 from app.context_processors import utility_text_processors
 from app.db import db
 from app.db.models import User
-# from app.error_handlers import error_handlers
-# from app.logging_config import log_con, LOGGING_CONFIG
+from app.error_handlers import error_handlers
+from app.logging_config import log_con, LOGGING_CONFIG
 from app.simple_pages import simple_pages
 # from app.songs import songs
 
@@ -48,8 +48,8 @@ def create_app():
     app.register_blueprint(database)
     # these load functionality without a web interface
 
-    # app.register_blueprint(log_con)
-    # app.register_blueprint(error_handlers)
+    app.register_blueprint(log_con)
+    app.register_blueprint(error_handlers)
     # app.register_blueprint(songs)
     app.context_processor(utility_text_processors)
     # add command function to cli commands
