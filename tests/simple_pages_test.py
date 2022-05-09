20e220e121918484885e38c7823e786480d48ed1
Harshil2830 <shahharshil2830@gmail.com>
2022-05-08 23:52:49 -0400

    Fixing simple pages test

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -5,7 +5,6 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-#   assert b'<a class="nav-link" href="/about">About</a>' in response.data
     assert b'href="/about"' in response.data
     assert b'href="/welcome"' in response.data
     assert b'href="/login"' in response.data
